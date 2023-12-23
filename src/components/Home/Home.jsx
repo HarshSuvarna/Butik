f0ae7c888f21cc21427f74134b0b826bef27555f
Harsh Suvarna <harsh.suvarna0@gmail.com>
2023-12-23 14:29:49 +0000

    added component routers

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,18 +4,82 @@ import { getUser } from "../../services/auth.services";
 import "./home.css";
 import { getNearestStores, getLocation } from "../../services/stores.services";
 import { getAllCategories } from "../../services/categories.services";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Categories from "./Category";
 import Stores from "./Stores";
+import Loader from "../loader/loader";
 
 const Home = ({ cookies }) => {
   const [categories, setCategories] = useState([]);
   const [stores, setStores] = useState([]);
-
   const [showMenu, setShowMenu] = useState(false);
+
+  const path = window.location.pathname;
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const renderContent = () => {
+    // switch (path) {
+    //   case "/home":
+    //     return (
+    //       <>
+    //         <Categories categories={categories} />
+    //         <Stores stores={stores} />
+    //       </>
+    //     );
+    //   //   return <HomeContent />;
+    //   // case "category":
+    //   //   return <CategoryContent />;
+    //   // case "chat":
+    //   //   return <ChatContent />;
+    //   // case "account":
+    //   //   return <AccountContent />;
+    //   default:
+    //     window.history.pushState({}, "", "/home");
+    // }
+    return (
+      <Routes>
+        <Route
+          path="/home"
+          exact
+          element={
+            <>
+              <Categories categories={categories} />
+              <Stores stores={stores} />
+            </>
+          }
+        />
+        <Route path="/categories" exact element={<Loader />} />
+        <Route
+          path="/chat"
+          exact
+          element={
+            <>
+              <Categories categories={categories} />
+              <Stores stores={stores} />
+            </>
+          }
+        />
+        <Route
+          path="/account"
+          exact
+          element={
+            <>
+              <Categories categories={categories} />
+              <Stores stores={stores} />
+            </>
+          }
+        />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    );
+  };
+
+  useEffect(() => {
+    console.log("path :>> ", path);
+  }, [path]);
 
   const fetchData = async () => {
     try {
@@ -57,19 +121,43 @@ const Home = ({ cookies }) => {
           <button onClick={logout}>Logout</button>
         </div>
       </div>
+      <div className="body-content">
+        <div className={`side-bar ${showMenu && "expanded"}`}>
+          <i
+            className="fa-solid fa-house icon-container"
+            onClick={() => {
+              window.history.pushState({}, "", "/home");
+            }}
+          >
+            <p>Home</p>
+          </i>
+          <i
+            className="fa-solid fa-shapes"
+            onClick={() => {
+              window.history.pushState({}, "", "/categories");
+            }}
+          >
+            Categories
+          </i>
+          <i
+            className="fa-solid fa-comment"
+            onClick={() => {
+              window.history.pushState({}, "", "/chat");
+            }}
+          >
+            Chat
+          </i>
+          <i
+            className="fa-solid fa-user"
+            onClick={() => {
+              window.history.pushState({}, "", "/account");
+            }}
+          >
+            Account
+          </i>
+        </div>
 
-      <div className={`side-bar ${showMenu && "expanded"}`}>
-        <i className="fa-solid fa-house icon-container">
-          <p>Home</p>
-        </i>
-        <i className="fa-solid fa-shapes">Categories</i>
-        <i className="fa-solid fa-comment">Chat</i>
-        <i className="fa-solid fa-user">Account</i>
-      </div>
-
-      <div className="category-store-container">
-        <Categories categories={categories} />
-        <Stores stores={stores} />
+        <div className="category-store-container">{renderContent()}</div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import axiosInterceptors from "./common/axiosInterceptors.js";
 import Cookies from "universal-cookie";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { MyProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const cookies = new Cookies();
 axiosInterceptors(axios, cookies);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MyProvider>
+  // <React.StrictMode>
+  <MyProvider>
+    <BrowserRouter>
       <App cookies={cookies} />
-    </MyProvider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </MyProvider>
+  // </React.StrictMode>
 );
