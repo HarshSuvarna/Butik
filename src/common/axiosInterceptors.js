92a6dcc7602cb105a8066df5baa1eca3919ddbb8
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-01-07 21:50:21 +0000

    URL change

--- src/common/axiosInterceptors.js
+++ src/common/axiosInterceptors.js
@@ -1,5 +1,5 @@
 export default function axiosInterceptors(axios, cookies) {
-  axios.defaults.baseURL = "https://test-service-emu7ybkl6a-uc.a.run.app"; //import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.baseURL = "https://test-service-emu7ybkl6a-el.a.run.app/"; //import.meta.env.VITE_API_BASE_URL;
 
   axios.interceptors.request.use((request) => {
     let token = cookies?.cookies?.jwt_authorization;
