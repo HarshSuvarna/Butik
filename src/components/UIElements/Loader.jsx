e1293ebdaefc39510aaa4881c686bb070b9accc9
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-01-09 21:25:19 +0000

    test 4

--- src/components/UIElements/Loader.jsx
+++ src/components/UIElements/Loader.jsx
@@ -1,7 +1,7 @@
 import "./loader.css";
 // import React from "react";
 
-export const Loader = ({ dim }) => {
+const Loader = ({ dim }) => {
   return <span style={{ height: "24px", width: "24px" }} className="loader" />;
 };
 
