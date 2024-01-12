4d78b7f95a5d7ad6b0f5100c2bb52ea60d8ecf41
Harsh Suvarna <harsh.suvarna0@gmail.com>
2024-01-12 13:45:19 +0000

    added env secrest

--- src/common/axiosInterceptors.js
+++ src/common/axiosInterceptors.js
@@ -1,6 +1,5 @@
 export default function axiosInterceptors(axios, cookies) {
-  axios.defaults.baseURL = "https://test-service-emu7ybkl6a-el.a.run.app/"; //import.meta.env.VITE_API_BASE_URL;
-
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
   axios.interceptors.request.use((request) => {
     let token = cookies?.cookies?.jwt_authorization;
     request.headers["authorization"] = token ? "Bearer " + token : "";
--- test.js
+++
@@ -1,3 +0,0 @@
-let ss = "Indian-&-Festive-Wear.png";
-let sss = ss.replace("-&-", "_").replace("-", "_").replace(".png", "");
-console.log("sss :>> ", sss);
